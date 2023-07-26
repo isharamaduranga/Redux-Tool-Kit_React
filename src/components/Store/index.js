833a489c74bd83757366a7ecb0317341d64684e3
Ishara Maduranga <99071775+isharamaduranga@users.noreply.github.com>
2023-07-26 15:47:08 +0530

    Added redux toolkit and updated program using createSlice function..

--- src/components/Store/index.js
+++ src/components/Store/index.js
@@ -1,38 +1,28 @@
 import {createStore} from "redux";
-
+import {createSlice} from '@reduxjs/toolkit'
 
 const initialState = {counter: 0, showCounter: true};
 
+const countSlice = createSlice({
+    name:'counter',
+    initialState:initialState,
+    reducers:{
+        increment(state){
+            state.counter++;
+        },
+        decrement(state){
+            state.counter--;
+        },
+        increase(state,action){
+            state.counter=state.counter+action.amount;
+        },
+        toggleCounter(state){
+            state.showCounter=!state.showCounter;
+        }
+    }
+});
 
-const countReducer = (state = initialState, action) => {
 
-    if (action.type === 'increment') {
-        return {
-            counter: state.counter + 1,
-            showCounter: state.showCounter
-        }
-    }
-    if (action.type === 'decrement') {
-        return {
-            counter: state.counter - 1,
-            showCounter: state.showCounter
-        }
-    }
-    if (action.type === 'increase') {
-        return {
-            counter: state.counter + action.amount,
-            showCounter: state.showCounter
-        }
-    }
-    if (action.type === 'toggle') {
-        return {
-            counter: state.counter,
-            showCounter: !state.showCounter
-        }
-    }
-    return state;
-}
-
-const store = createStore(countReducer);
+const store = createStore(countSlice.reducer);
 
 export default store
